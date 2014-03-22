b31b07dbd732a97d7bc90b4a2ec977f09042a95b
euleong <el@eugenialeong.com>
2014-03-22 10:14:05 -0700

    Initial Commit

---
+++ yelpSearch/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  yelpSearch
+//
+//  Created by Eugenia Leong on 3/22/14.
+//  Copyright (c) 2014 Eugenia Leong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ yelpSearchTests/yelpSearchTests.m
@@ -0,0 +1,34 @@
+//
+//  yelpSearchTests.m
+//  yelpSearchTests
+//
+//  Created by Eugenia Leong on 3/22/14.
+//  Copyright (c) 2014 Eugenia Leong. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface yelpSearchTests : XCTestCase
+
+@end
+
+@implementation yelpSearchTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
